3bb7737db457a5a589f31133b1cce3216f0b6c4b
Ryuichi Nakano <170441114@ccalumni.meijo-u.ac.jp>
2021-01-16 06:38:29 +0900

    2章完了

---
+++ 2/2-11/app.js
@@ -0,0 +1,70 @@
+var items=[
+  {
+    name:'鉛筆',
+    price:300,
+    quantity:0
+  },
+  {
+    name:'ノート',
+    price:400,
+    quantity:0
+  },
+  {
+    name:'消しゴム',
+    price:500,
+    quantity:0
+  },
+]
+
+var vm = new Vue({
+  el:'#app',
+  data:{
+    items: items
+  },
+  filters:{
+    numberWithDelimiter: function(value){
+      if(!value){
+        return '0'
+      }
+      return value.toString().replace(/(\d)(?=(\d{3})+$)/g,'$1,')
+    }
+  },
+
+  methods:{
+    doBuy:function(){
+      //本来はここでサーバと通信を行う
+      alert(this.totalPriceWithTax + '円のお買い上げ！')
+      this.items.forEach(function(item){
+        item.quantity=0
+      })
+    }
+  },
+
+  computed:{
+    totalPrice: function(){
+      return this.items.reduce(function (sum,item){
+        return sum + (item.price * item.quantity)
+      },0)
+    },
+    totalPriceWithTax:function(){
+      return Math.floor(this.totalPrice * 1.10)
+    },
+    canBuy : function(){
+      return this.totalPrice >= 1000
+    },
+    errorMessageClass: function(){
+      return {
+        error : !this.canBuy
+      }
+    },
+    errorMessageStyle: function(){
+      // canBuyがfalseで赤く表示
+      return{
+        border : this.canBuy ? '' : '1px solid red',
+        color : this.canBuy ? '' : 'red'
+      }
+    }
+  }
+})
+
+window.vm = vm
